3507de415b6a8e5d5e2911f95165b9abe9d63de0
Wellington <hanzo.hasashi.pmk@gmail.com>
2022-03-07 16:08:09 -0300

    Bug fix

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -36,6 +36,7 @@ const GlobalStyle = createGlobalStyle`
     background-size: cover;
     background-position: center center;
     background-attachment: fixed;
+    overflow-x: hidden;
   }
 
   br {
